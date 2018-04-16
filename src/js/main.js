5aa7198701266d4e3574ebcb76254ff83abded65
John Lai <jlai@carpages.ca>
2018-04-16 16:01:06 -0400

    playing jquery

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,30 @@
-console.log("JS load");
+console.log('JS load');
+
+$('.bluebox').click(function() {
+	$(this).hide(2000);
+});
+
+//slide show
+var slideShow = ['coby1.png', 'coby2.png', 'coby3.png', 'coby4.png'];
+
+var focusImg = 1;
+
+$('#slider').click(function() {
+	var image = slideShow[focusImg];
+	focusImg++;
+
+	if (focusImg > 3) {
+		focusImg = 0;
+	}
+
+	$('#slider').attr('src', '/dist/img/' + image);
+});
+
+//jquery ui menu
+
+$('#shMenu').menu({
+	position: {
+		my: 'center top',
+		at: 'center bottom'
+	}
+});
